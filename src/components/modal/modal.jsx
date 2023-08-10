ddd18d24470a65028324905077d0cebc3dae1d50
Dexxter300 <daniilsafarov2005@gmail.com>
2023-08-10 11:23:28 +0300

    Update modal.jsx

--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -5,12 +5,6 @@ import css from './modal.module.css';
 const modalRoot = document.querySelector('#modal-root');
 
 export const Modal = ({ img, onClose }) => {
-  //   static defaultProps = {
-  //     img: {
-  //       largeImageURL:
-  //         'https://pixabay.com/get/g9453aa195c07bc56268891e4851ed3e51098d17674c0e9277d44c35fde7174acf83971f02dd6b5d09ce48a119ecd992ee910fe4d81f6f2a1bbb3d4f126c755a2_1280.jpg',
-  //     },
-  //   };
   useEffect(() => {
     window.addEventListener('keydown', hendleKeyDown);
 
